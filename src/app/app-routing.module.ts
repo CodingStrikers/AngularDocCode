bc9f0dec68558b732e9cf99b9b8d4fea74b76934
apple <apple@apples-MacBook-Pro-2.local>
2020-07-14 10:16:58 +0530

    Angularfilereadytobeuploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VisionComponent } from './vision/vision.component';
+import { combineLatest } from 'rxjs';
+import { AppComponent } from './app.component';
+import { DocloginComponent } from './doclogin/doclogin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'vision',
+    component:VisionComponent
+  },
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:"doctorlogin/:id",
+    component:DocloginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
